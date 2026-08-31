da2509a7c508203e6da5021e81ae31684fabe48e
Aastha <aasthakushwaha644@gmail.com>
2026-08-31 15:00:09 +0530

    Delete unwanted files

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('button')
-btn.addEventListener('mouseenter',function(){
-  document.body.style.backgroundColor = "yellow"
-})
-btn.addEventListener("mouseleave", function () {
-    document.body.style.backgroundColor = "slategray"
-});
